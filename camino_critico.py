34a978a02f6d47f912f45df91a28f0eb20503d24
Carlos Freitas <47233287+CarlosSoki@users.noreply.github.com>
2021-12-02 07:39:01 -0400

    Update camino_critico.py

--- camino_critico.py
+++ camino_critico.py
@@ -1,10 +1,29 @@
 def cpm(actividades):
-
+    #CREAR TABLA CPM E IMPRIMIRLA
     tabla_cpm = crear_tabla_cpm(actividades)
-    tabla_sucesor = crear_tabla_sucesor(actividades)
+    print("\n|----------------Inicialización Tabla CPM---------------|")
+    for cpm in tabla_cpm:
+        print(cpm)
 
+    #CREAR TABLA SUCESORES E IMPRIMIRLA
+    tabla_sucesor = crear_tabla_sucesor(actividades)
+    print("\n|-----------Inicialización Tabla De SUCESORES-----------|")     
+    for suce in tabla_sucesor:
+        print(suce)   
+
+    #HACER FORWARD EN LA TABLA CPM E IMPRIMIRLA
+    print("\nCORRIENDO FORWARD ...")
     tabla_cpm = forward(tabla_cpm, tabla_sucesor, actividades)
-    
+    print("\n|----------------Forward Tabla CPM---------------|")
+    for cpm in tabla_cpm:
+        print(cpm)
+
+    #HACER BACKWARD EN LA TABLA CPM E IMPRIMIRLA
+    tabla_cpm = backward(tabla_cpm, actividades, tabla_sucesor)
+    print("\n|----------------Backward Tabla CPM---------------|")
+    for cpm in tabla_cpm:
+        print(cpm)
+
     return tabla_cpm
 
 def crear_tabla_cpm(actividades):
@@ -22,7 +41,6 @@ def crear_tabla_cpm(actividades):
             "LF": None, 
             "holgura": None})
 
-    
     return tabla_cpm
 
 def crear_tabla_sucesor(actividades):
@@ -30,35 +48,30 @@ def crear_tabla_sucesor(actividades):
     tabla_sucesor = []
 
     for actividad in actividades:
-    
         sucesor = actividad['numero_act']
 
         for predecesor in actividad['predecesor']:
             tabla_sucesor.append({"numero_act": predecesor, "sucesor": sucesor})
-    print(tabla_sucesor)    
+
     return tabla_sucesor
 
 def forward(tabla_cpm, tabla_sucesor, actividades):
 
     cola = []
-
     #agregamos las actividades iniciales a la cola
-
     for actividad in actividades:
-        
         if len(actividad['predecesor']) == 0:
-            
             cola.append(actividad)
     
-
     #mientras queden actividades
     while len(cola) > 0:
         nodo = cola.pop(0)
         print("Evaluando el nodo: " + str(nodo))
 
         #buscamos cuales son sus sucesores
+        print("-Buscando sus sucesores:")
         sucesores = list(filter(lambda x: x['numero_act'] == nodo['numero_act'], tabla_sucesor))
-        print("Sucesores: " + str(sucesores))
+        print("-Sucesores: " + str(sucesores))
 
         #agregamos los sucesores a la cola
         for sucesor in sucesores:
@@ -67,6 +80,7 @@ def forward(tabla_cpm, tabla_sucesor, actividades):
         ES = 0
         EF = 0
         #Calculamos el ES buscando el maximo EF de los predecesores
+        print("-Calculando ES y EF ... \n")
         if len(nodo['predecesor']) > 0:
             for pred in nodo['predecesor']:
                 if tabla_cpm[pred]['EF'] > ES:
@@ -77,8 +91,6 @@ def forward(tabla_cpm, tabla_sucesor, actividades):
         #Calculamos el EF sumando la duracion al ES
         tabla_cpm[nodo['numero_act']]['EF'] = ES + tabla_cpm[nodo['numero_act']]['duracion']
 
-    print(tabla_cpm)
-    tabla_cpm = backward(tabla_cpm, actividades, tabla_sucesor)
     return tabla_cpm
 
 def backward(tabla_cpm, actividades, tabla_sucesor):
@@ -92,9 +104,6 @@ def backward(tabla_cpm, actividades, tabla_sucesor):
     for actividad in tabla_cpm:
         if len(list(filter(lambda x: x['numero_act'] == actividad['numero_act'], tabla_sucesor))) == 0:
             cola.append(actividad)
-    #actividad_final = list(filter(lambda x: x['EF'] == duracion_proyecto, tabla_cpm))
-    #print("Actividad Final: " + str(actividad_final))
-    #cola = actividad_final
 
     while len(cola) > 0:
         nodo = cola.pop(0)
@@ -137,6 +146,14 @@ def calcular_camino_critico(tabla_cpm):
     for actividad in tabla_cpm:
         if actividad['holgura'] == 0:
             ruta_critica.append(actividad['actividad'])
+    
+    print("\nLas actividades en la ruta critica son: " + str(ruta_critica))
+    return ruta_critica
 
-    print("Las actividades en la ruta critica son: " + str(ruta_critica))
-    return
+def cpm_gui(actividades):
+    tabla_cpm = crear_tabla_cpm(actividades)
+    tabla_sucesor = crear_tabla_sucesor(actividades)
+    tabla_cpm = forward(tabla_cpm, tabla_sucesor, actividades)
+    tabla_cpm = backward(tabla_cpm, actividades, tabla_sucesor)
+
+    return tabla_cpm
