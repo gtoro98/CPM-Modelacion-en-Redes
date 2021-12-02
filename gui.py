fc782f15676dff71ba8827fbcb6e18b472801f34
Carlos Freitas <47233287+CarlosSoki@users.noreply.github.com>
2021-12-02 07:39:33 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,219 +4,250 @@ from tkinter import ttk
 from time import *
 from tkinter import font
 from camino_critico import*
-
-
-
-
+from grafo import*
 
 f = open("temp.txt","w+")
-
-def close_window(main_window):
-    main_window.destroy()
 
 def close_new_window(new_window):
     new_window.destroy()
 
+def close_window(main_window):
+    main_window.destroy()
+
 def read():
     f = open("temp.txt","r")
     string = ""
-
     string = f.readline()
     f.close()
     return string
 
-
-
-def write_string_on_temp(id, description, duration,predecesor_field, window1, counter, frame):
+def write_string_on_temp(description, duration,predecesor_field, window1, counter):
     string = read()
-    counter = counter + 1
+    
     if predecesor_field.get() == "":
         predecesor = ""    
     else:
         predecesor = predecesor_field.get()
     
     string1 = string + str(counter) + "-" + description.get() + "-" + duration.get() + "-" + predecesor + ";"
+    counter = counter + 1
+
+    print("Actividad "+str(counter)+":  Descripción: "+ description.get() +"  Duración: "+str(duration.get())+"  Predecesor: "+str(predecesor))
 
     f = open("temp.txt","w+")
     f.write(string1)
     f.close()
     window1.destroy()
-    desition = messagebox.askyesno(message="Desea agregar otra actividad?")
+
+    desition = messagebox.askyesno(message="¿Desea agregar otra actividad?")
     if desition == True:
-        activity(counter, frame)
+        activity(counter)
     else:
         messagebox.showinfo(message="Se han agregado las actividades de forma satisfactoria.")
 
-
-
-def get_critical_route():
-
-    string = read()
-    actividades = []
-
-
-
-    a = string.split(";")
-
-
-    for i in range(0,len(a)-1):
-       
-        if "," in a[i].split("-")[3]:
-            predecesors = a[i].split("-")[3].split(",")
-            actividades.append({"numero_act": int(a[i].split("-")[0]), "descripcion": a[i].split("-")[1], "duracion": a[i].split("-")[2],"predecesor": predecesors})
-        else:
-            actividades.append({"numero_act": int(a[i].split("-")[0]), "descripcion": a[i].split("-")[1], "duracion": a[i].split("-")[2],"predecesor": a[i].split("-")[3]})
-
-
-    print(actividades)
-    cpm(actividades)
-
-def activity(counter, frame): 
+#VENTANA AGREGAR ACTIVIDADES ##############################################################################################################
+def activity(counter): 
 
     window1 = Tk()
+    frame = Frame(window1).place(x = 0, y = 0)
     window1.title("Agregar actividades")
     window1.geometry("800x400")
+    window1.config(background="#FAB382")
 
     message0 = Label(window1, 
-                    text="Indique un identificador para la actividad:", 
-                    font=("Arial",14,"bold"),
+                    text="INGRESE LOS DATOS DE LA ACTIVIDAD", 
+                    background="#FAB382", 
+                    font=("Arial",18,"bold"),
                     justify="center",
-                    ).place(x=250,y=20)
-
-    message1 = Label(window1, 
-                    text="Indique un identificador para la actividad:", 
-                    font=("Arial",10,),
-                    justify="right",
-                    ).place(x=20,y=80)
-    
-    id = Entry(window1, width= 15)
-    id.place(x=270,y=80)
+                    ).place(x=150,y=20)
 
     message2 = Label(window1, 
-                    text="Ingrese una descripción para la actividad:", 
-                    font=("Arial",10,),
-                    justify="right",
-                    ).place(x=20,y=110)
+                    text="Ingrese un nombre para la actividad:",
+                    background="#FAB382",  
+                    font=("Arial",14,),
+                    justify="center",
+                    ).place(x=150,y=110)
 
-    description = Entry(window1, width= 15)
-    description.place(x=270,y=110)
+    description = Entry(window1, width= 15, font=font.Font(size=12))
+    description.place(x=475,y=115)
 
     message3 = Label(window1, 
                     text="Ingrese la duración de la actividad:", 
-                    font=("Arial",10,),
+                    background="#FAB382", 
+                    font=("Arial",14,),
                     justify="right",
-                    ).place(x=20,y=140)
-    duration = Entry(window1, width= 15)
-    duration.place(x=270,y=140)
+                    ).place(x=150,y=155)
 
-    combobox = ttk.Combobox(window1, width =10)
-    combobox["state"] = "readonly"
-    combobox.place(x= 370, y= 138)
-    combobox["values"] = ("","Días","Semanas","Meses")
-    combobox.current(0)
+    duration = Entry(window1, width= 15, font=font.Font(size=12))
+    duration.place(x=475,y=155)
 
     message4 = Label(window1, 
-                    text="Ingrese el predecesor: ", 
-                    font=("Arial",10,),
+                    text="Ingrese los predecesor usando ( , ): ", 
+                    background="#FAB382", 
+                    font=("Arial",14,),
                     justify="right",
-                    ).place(x=20,y=168)
+                    ).place(x=150,y=195)
 
-    predecesor_field = Entry(window1, width =10)
-    predecesor_field.place(x= 250, y= 168)
+    predecesor_field = Entry(window1, width= 15, font=font.Font(size=12))
+    predecesor_field.place(x= 475, y= 195)
 
-    add = Button(window1, text="Agregar actividad", command= lambda: write_string_on_temp(id, description, duration,predecesor_field, window1, counter, frame)).place(x=350, y=350)
-                
-        
+    add = Button(window1, text="Agregar Actividad", command= lambda: write_string_on_temp(description, duration,predecesor_field, window1, counter),  width =20, bg='#FF5733', fg='#ffffff', font=font.Font(size=22, weight='bold')).place(x=300, y=280)
+             
 
 def main_window_function():
     
     counter = 0
+    actividades = []
+    
+    def get_critical_route():
+        string = read()
+        a = string.split(";") #['0-Cemento-2-', '1-Mufa-2-2,1', '']
+        actividades.clear()
+
+        for i in range(0,len(a)-1): #0,1
+            if "," in a[i].split("-")[3]: # Picas a en 4 pedazos. ['0', 'cemento','2',""]  ['1','mufa','2','2,1'] array de 0-3 posiciones
+                predecesors = list(map(int, a[i].split("-")[3].split(",")))
+            else:
+                predecesors = list(map(int, a[i].split("-")[3]))
+            actividades.append({"numero_act": int(a[i].split("-")[0]), "descripcion": a[i].split("-")[1], "duracion": int(a[i].split("-")[2]),"predecesor": predecesors})
+
+        print("\n|------Actividades Ingresadas------|")
+        for acti in actividades:
+            print(acti)
+
+        CPM=cpm(actividades)
+
+        RC = calcular_camino_critico(CPM)
+        label_rutaCritica= "Las actividades en la ruta critica son:\n["+str(RC[0])+"]"
+        for i in range(len(RC)-1):
+            label_rutaCritica = label_rutaCritica + "➜["+str(RC[i+1])+"]"
+        labelRutaCritica.config(text=label_rutaCritica, background="light green") #se llena el label con el tiempo
+
+        crearGrafo(CPM, actividades,RC)
 
     def update_time():
         time = strftime("Hora: " + "%I:%M:%S %p")
-        time_label.config(text=time, background="light grey")
+        time_label.config(text=time, background="light blue")
         time_label.after(1000,update_time)
         day = strftime("%A")
-        day_label.config(text=day, background="light grey", )
+        day_label.config(text=day, background="light blue", )
+
+    #ABRIR VENTANA CON ACTIVIDADES ###############################################################################  
+    def openNewWindowActividades():
+        newWindow = Toplevel(main_window)
+        newWindow.title("Lista de Actividades:")
+        newWindow.geometry("700x700")
+        newWindow.config(background="#DB6060")
+
+        label_verAct=""
+        for i in range(len(actividades)):
+            label_verAct =label_verAct+str(actividades[i])+"\n"
+
+        label_verAct = Label(newWindow,  
+                    text=label_verAct, 
+                    background="#DB6060", 
+                    font=("Arial",12,"bold"),
+                    justify="left",
+                    )
+        label_verAct.place(x=0, y=0)
+    
+    #ABRIR VENTANA CON TABLA CPM ###############################################################################  
+    def openNewWindowTablaCPM():
+        newWindow = Toplevel(main_window)
+        newWindow.title("Tabla CPM:")
+        newWindow.geometry("800x700")
+        newWindow.config(background="#A0C4F0")
+
+        if(len(actividades)>0):
+            label_verCPMtext=""
+            tableCPM = cpm_gui(actividades)
+            for i in range(len(tableCPM)):
+                label_verCPMtext =label_verCPMtext+str(tableCPM[i])+"\n"
+
+            label_verCPM = Label(newWindow,  
+                        text=label_verCPMtext, 
+                        background="#A0C4F0", 
+                        font=("Arial",12,"bold"),
+                        justify="left",
+                        )
+            label_verCPM.place(x=0, y=0)
+
+    #ABRIR MOSTRAR GRAFO ###############################################################################  
+    def openNewWindowGrafo():
+        if(len(actividades)>0):
+            tabla_cpm=cpm_gui(actividades)
+            RutaMasCorta=calcular_camino_critico(tabla_cpm)
+            crearGrafo(tabla_cpm, actividades,RutaMasCorta)
 
     #CREAR PANTALLA Y DETALLES DE LA MISMA #########################################################################################
     main_window = Tk()
     frame = Frame(main_window).place(x = 0, y = 0)
     main_window.geometry("800x600")
     main_window.title("CPM - Ruta Crítica")
-    main_window.config(background="light grey")
+    main_window.config(background="light blue")
 
 
     #HORA Y FECHA CONFIGURACION #####################################################################################################
     time_label = Label(frame, font=("Arial",10), # tiempo
-                        background = "light grey",
+                        background = "light blue",
                         justify= "right")
-    time_label.place(x=680,y=0)
+    time_label.place(x=0,y=0)
 
     day_label = Label(frame, font=("Arial",10), 
-                        background = "light grey",
+                        background = "light blue",
                         justify = "right")
-    day_label.place(x=703, y=20)
+    day_label.place(x=0, y=20)
 
     update_time()
 
     #BOTONES ##############################################################################################################
+    close_button = Button(frame, text="X", font=font.Font(size=15, weight='bold'), width=3, command= lambda: close_window(main_window), bg='red').pack(side=TOP, anchor=E)
 
-   
-    add_activity = Button(frame, text="Agregar actividades", font=font.Font(size=10), command= lambda: activity(counter, frame)).place(x=350, y=550)
-    critical_route = Button(frame, text=" Hallar ruta critica", font=font.Font(size=10), command= get_critical_route).place(x=350, y=450)
+    add_activity_button = Button(main_window, text="Agregar actividades", command= lambda: activity(counter), width =16, bg='#FF5733', fg='#ffffff', font=font.Font(size=13, weight='bold')).place(x=290, y=180)
+
+    critical_route_button = Button(main_window, text=" Hallar CPM", command= get_critical_route, width =16, bg='#900C3F', fg='#ffffff', font=font.Font(size=13, weight='bold')).place(x=290, y=220)
+
+    verActividades_button = Button(main_window, text="Ver Actividades", command=openNewWindowActividades, width =13, bg='#C70039', fg='#ffffff', font=font.Font(size=14, weight='bold')).place(x = 80, y = 500) 
+
+    verTablaCPM_button = Button(main_window, text="Tabla CPM",  command=openNewWindowTablaCPM, width =13, bg='#0052cc', fg='#ffffff', font=font.Font(size=14, weight='bold')).place(x = 300, y = 500)
+
+    verGrafo_button = Button(main_window, text="Ver Grafo",  command=openNewWindowGrafo, width =13, bg='#FFC300', fg='#ffffff', font=font.Font(size=14, weight='bold')).place(x = 520, y = 500)
 
     #LABEL TITULO ##########################################################################################################
     labelTitulo = Label(frame,  #titulo
-                    text="CPM - Ruta crítica.", 
-                    background="light grey", 
-                    font=("Arial",16,"bold"),
+                    text="CPM - Critical Path Method", 
+                    background="light blue", 
+                    font=("Arial",18,"bold"),
                     justify="center",
                     )
-    labelTitulo.place(x = 300, y = 60)
+    labelTitulo.place(x = 240, y = 60)
 
     #LABEL MENSAJEs ##########################################################################################################
 
     message1 = Label(frame,  
                     text="El método de la ruta crítica o diagrama CPM (Critical Path Method) por sus siglas en inglés\n es un algoritmo basado en la teoría de redes que permite calcular el tiempo mínimo de realización de un proyecto.", 
-                    background="light grey", 
-                    font=("Arial", 10),
+                    background="light blue", 
+                    font=("Arial", 12),
                     justify="center",
                     )
 
-    message1.place(x= 70,y=100)
+    message1.place(x= 0,y=100)
 
     message2 = Label(frame,  
-                    text="Esta herramienta permitirá organizar tus proyectos de manera eficiente", 
-                    background="light grey", 
-                    font=("Arial", 10),
-                    justify="center",
+                    text="Para comenzar: \n\n1- Agrege Actividades:\n\n2- Halle su ruta critica:", 
+                    background="light Blue", 
+                    font=("Arial", 12, "bold"),
+                    justify="left",
                     )
 
-    message2.place(x=70,y=200)
-
-    message3 = Label(frame,  
-                    text="Para comenzar, por favor indica en número de actividades que componen el proyecto: ", 
-                    background="light grey", 
-                    font=("Arial", 10),
-                    justify="center",
-                    )
-
-    message3.place(x=70,y=250)
+    message2.place(x=100,y=150)
 
     #LABEL TIEMPO INFO #####################################################################################################
-
-    spinner_activities = ttk.Spinbox(from_=0, to=10000, increment=1, state="readonly")  ## *****************quitar el readonly
-    spinner_activities.place(x=600, y=250, width=50)
-
-
-
-    #LABEL TIEMPO INFO #####################################################################################################
-    labelTiempo = Label(frame,text="",
-                        background="light grey", 
+    labelRutaCritica = Label(frame,text="",
+                        background="light blue", 
                         font=("Arial",18,"bold italic"),
                         justify="center",)
-    labelTiempo.place(x=140,y=340) 
+    labelRutaCritica.place(x=100,y=340) 
 
     main_window.mainloop()
 
